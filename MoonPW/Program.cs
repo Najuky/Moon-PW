7066cd63f6ef2546c9679e38ef614ffe7d8ce506
!94#6666 <68169550+Najuky@users.noreply.github.com>
2022-07-31 00:05:32 +0200

    Main Build ig

---
+++ MoonPW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoonPW
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hey there! This is a password generator.\n");
+        }
+    }
+}
